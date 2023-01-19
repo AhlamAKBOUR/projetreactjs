1960d3f343813b697d638bd97ec980ea46e11126
AhlamAKBOUR <119418430+AhlamAKBOUR@users.noreply.github.com>
2023-01-19 14:36:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Formulaire from "./Formulaire";
+import React from "react";
+import "./styles.css";
+function App() {
+  return (
+    <div className="App">
+      <Formulaire></Formulaire>
+    </div>
+  );
+}
+export default App;
